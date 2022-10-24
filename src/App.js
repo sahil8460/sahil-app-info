b1c11f850e50ef4f1e103f0e3e51afe2cefecadb
sahil8460 <akbarisahil8460@gmail.com>
2022-10-24 12:55:46 +0530

    change api

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     const [data, setData] = React.useState(null);
 
     React.useEffect(() => {
-        fetch("http://localhost:3001/api")
+        fetch("https://jsonplaceholder.typicode.com/todos")
             .then((res) => res.json())
             .then((data) => {
                 console.log("message", data);
